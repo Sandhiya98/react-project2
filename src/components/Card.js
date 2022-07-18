d012b5e19e561bc4235bd1808c2987655e579256
sandhiyadurai <sandhiya.durai26@gmail.com>
2022-07-18 23:59:24 +0530

    commit my changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Card.css';
 // this is an example comment
+//comment from sandy
 function Card(props){
     const classes ='Card' + props.className;
     return<div className={classes}>{props.children}</div>
